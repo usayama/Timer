061b41e6965f76bbfd266e9f424b5cbfe72ef45a
y-nakane <y-nakane@sakura.ad.jp>
2018-11-16 17:09:07 +0900

    画面を更新する関数を追加

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -29,13 +29,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var countDownLabel: UILabel!
     
     @IBAction func settingButtonAction(_ sender: UIBarButtonItem) {
-        
     }
     @IBAction func startButtonAction(_ sender: UIButton) {
-        
     }
     @IBAction func stopButtonAction(_ sender: UIButton) {
-        
+    }
+    
+    // 画面の更新をする処理
+    func displayUpdate() -> Int {
+        // UserDefaultsインスタンスを生成
+        let settings = UserDefaults.standard
+        // 取得した秒数をtimerValueにわたす
+        let timerValue = settings.integer(forKey: settingKey)
+        // remainCount（残り時間）を生成
+        let remainCount = timerValue - count
+        // remainCount（残り時間）をラベルに表示
+        countDownLabel.text = "残り\(remainCount)秒"
+        // 残り時間を戻り値に設定
+        return remainCount
     }
 }
 
