e5643db586dfb950812403845f2b54f4b3f64578
y-nakane <y-nakane@sakura.ad.jp>
2018-11-16 16:20:06 +0900

    UserDefaultsに初期値を設定するコードを追加

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // タイマーの変数を作成
+    var timer: Timer?
+    // カウント（経過時間）の変数を作成
+    var count = 0
+    // 設定値を扱うキーを設定
+    let settingKey = "timer_value"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // UserDefaultsのインスタンスを生成
+        let settings = UserDefaults.standard
+        // UserDefaultsに初期値を設定
+        settings.register(defaults: [settingKey: 10])
     }
 
     @IBOutlet weak var countDownLabel: UILabel!
