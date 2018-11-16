af895b00928d52ff2192cd2a5e13461c8d9a6497
y-nakane <y-nakane@sakura.ad.jp>
2018-11-16 12:53:44 +0900

    決定ボタンをコードに接続

--- Timer/SettingViewController.swift
+++ Timer/SettingViewController.swift
@@ -29,4 +29,8 @@ class SettingViewController: UIViewController {
 
 
     @IBOutlet weak var timerSettingPicker: UIPickerView!
+    
+    @IBAction func decisionButtonAction(_ sender: UIButton) {
+        
+    }
 }
